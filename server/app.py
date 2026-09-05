81027e58850c5a9075cf846c7ee4603a9a0bcdfb
Burhanuddin siddikwala <bsiddikwala@gmail.com>
2026-09-05 15:27:16 +0530

    Fix Render port binding - use PORT env var

--- server/app.py
+++ server/app.py
@@ -101,4 +101,5 @@ def clear_history():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
